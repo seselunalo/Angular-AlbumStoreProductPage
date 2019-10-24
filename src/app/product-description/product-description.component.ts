4eb2192e1e5204854e8c2e9fd0842f6d2c616f8e
F Sesera <F.L. Sesera>
2019-10-24 15:45:07 +0300

    module 3 complete correction

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product-service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
+
 export class ProductDescriptionComponent implements OnInit {
   albumInfo: any;
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,13 +6,12 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class ProductService {
-  private _albumurl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) {}
-
 
   getAlbum(id: number) {
 
-    return this._http.get(this._albumurl).map((response: Response) => response.json());
+    return this._http.get(this._albumUrl).map((response: Response) => response.json());
   }
 
 }
